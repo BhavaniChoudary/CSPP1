8680351cadd1cb4559cbb000ef3a3df0dfe2a197
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-01 18:23:53 +0530

    hello_happy_world

---
+++ CSPP1_practice/m3/hello_happy_world.py
@@ -0,0 +1,8 @@
+'''
+@author : BhavaniChoudary
+This program prints 'hello world' if the value of an integer variable,
+happy, is strictly greater than 2.
+'''
+HAPPY = int(input())
+if HAPPY > 2:
+    print("hello world")
