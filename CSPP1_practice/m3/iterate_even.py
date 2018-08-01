255eb686fa7a29927cc0bdcb1e4af9df0dedab74
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-01 22:57:04 +0530

    iteration

---
+++ CSPP1_practice/m3/iterate_even.py
@@ -0,0 +1,14 @@
+'''
+ @author : shanmukhsurendra
+ IN this Program we use while, For loop to get
+ output even .
+'''
+Y = 2
+while Y <= 10:
+    print(Y)
+    Y = Y+2
+print('Goodbye!')
+
+for B in range(2, 12, 2):
+    print(B)
+print('Goodbye!')
