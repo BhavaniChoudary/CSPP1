faaba6224906ba4e6b387b55a8a25b20e468fad3
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-01 23:03:02 +0530

    even iteration reverse'

---
+++ CSPP1_practice/m3/iterate_even_reverse.py
@@ -0,0 +1,13 @@
+'''
+@author : shanmukhsurendra
+ IN this Program we use while, For loop to get
+ output  .
+ '''
+print('Hello!')
+X = 10
+while X >= 2:
+    print(X)
+    X = X-2
+print('Hello!')
+for X in range(10, 0, -2):
+    print(X)
