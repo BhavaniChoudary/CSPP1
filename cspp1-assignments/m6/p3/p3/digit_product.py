efaefdd91bff3f872a915e619c5244a81010c382
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-04 12:41:11 +0530

    " -> 0 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/m6/p3/p3/digit_product.py
+++ cspp1-assignments/m6/p3/p3/digit_product.py
@@ -11,17 +11,17 @@ def main():
     '''
     int_input = int(input())
     f_n = 0
-    if int_input < 0:
+    if int_input > 0:
         int_input = abs(int_input)
         f_n = 1
         digi_product = 1
         for i in range(len(str(int_input))):
             del i
-            k = int_input%10
+            k = int_input % 10
             digi_product = digi_product * k
             int_input = int_input // 10
             if f_n == 1:
-                print('-' + str(digi_product))
+                print(digi_product)
             else:
                 print(digi_product)
 
