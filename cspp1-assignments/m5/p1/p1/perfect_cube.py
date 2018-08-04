6a1056c333c7b14f906eb3e247550b609acb5aae
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-04 16:54:05 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- cspp1-assignments/m5/p1/p1/perfect_cube.py
+++ cspp1-assignments/m5/p1/p1/perfect_cube.py
@@ -21,9 +21,9 @@ def main():
         else:
             g_n += i_n
     if abs(g_n**3 - cube_num) >= e_n:
-        print(str(cube_num) + "is not a perfect cube")
+        print(str(cube_num) + " is not a perfect cube")
     else:
-        print(str(cube_num) + "is a perfect cube")
+        print(str(cube_num) + " is a perfect cube")
 
 if __name__ == "__main__":
     main()
