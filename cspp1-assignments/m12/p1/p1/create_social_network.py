f908c4fac11814847b81146b615acaa23d9a4161
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-11 12:16:04 +0530

    " -> 0 of 3 passed. pylint: 5.33/10 "

--- cspp1-assignments/m12/p1/p1/create_social_network.py
+++ cspp1-assignments/m12/p1/p1/create_social_network.py
@@ -42,10 +42,10 @@ def main():
     '''
         handling testcase input and printing output
     '''
-    L = []
+    L_a = []
     n = int(input())
     for i in range(n):
         list_a_input = input().split("follows")
-        L.extend(list_a_input)
-    print(create_social_network(L, n))
+        L_a.extend(list_a_input)
+    print(create_social_network(L_a, n))
 main()
