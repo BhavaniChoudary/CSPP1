98ce1da5b5ca49dfd91a875e8dc89d6a9f3973e1
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-11 12:06:54 +0530

    " -> 0 of 3 passed. pylint: 3.33/10 "

--- cspp1-assignments/m12/p1/p1/create_social_network.py
+++ cspp1-assignments/m12/p1/p1/create_social_network.py
@@ -1,54 +1,54 @@
 '''
-    Assignment-1 Create Social_a Network
+    Assignment-1 Create Social Network
 '''
 
-def create_social_a_network(list_a, n_a):
-    '''
-    Social network
-    '''
+def create_social_network(List, n):
     no_dict = {}
-    for i in range(0, len(list_a), 2):
-        if list_a[i] not in no_dict:
-            no_dict[list_a[i]] = list_a[i+1].split(",")
+    for i in range(0, len(List), 2):
+        if List[i] not in no_dict:
+            no_dict[List[i]] = List[i+1].split(",")
     return no_dict
 
     '''
         The data argument passed to the function is a string
-        It represents simpl_ae social_a network data
-        In this social_a network data there are peopl_ae fol_al_aowing other peopl_ae
+        It represents simple social network data
+        In this social network data there are people following other people
 
-        Here is an exampl_ae social_a network data string:
-        John fol_al_aows Bryant,Debra,Wal_ater
-        Bryant fol_al_aows Ol_aive,Ol_al_aie,Freda,Mercedes
-        Mercedes fol_al_aows Wal_ater,Robin,Bryant
-        Ol_aive fol_al_aows John,Ol_al_aie
+        Here is an example social network data string:
+        John follows Bryant,Debra,Walter
+        Bryant follows Olive,Ollie,Freda,Mercedes
+        Mercedes follows Walter,Robin,Bryant
+        Olive follows John,Ollie
 
-        The string has mul_atipl_ae l_aines and each l_aine represents one person
-        The first word of each l_aine is the name of the person
-        The second word is fol_al_aows that separates the person from the fol_al_aowers
-        After the second word is a l_aist_a of peopl_ae separated by ,
+        The string has multiple lines and each line represents one person
+        The first word of each line is the name of the person
+        The second word is follows that separates the person from the followers
+        After the second word is a list of people separated by ,
 
-        create_social_a_network function shoul_ad spl_ait the string on l_aines
-        then extract the person and the fol_al_aowers by spl_aitting each l_aine
-        final_al_ay add the person and the fol_al_aowers to a dictionary and
+        create_social_network function should split the string on lines
+        then extract the person and the followers by splitting each line
+        finally add the person and the followers to a dictionary and
         return the dictionary
 
-        Caution: watch out for trail_aing spaces whil_ae spl_aitting the string.
-        It may cause your test cases to fail_a al_athough your output may be right
+        Caution: watch out for trailing spaces while splitting the string.
+        It may cause your test cases to fail although your output may be right
 
-        Error handl_aing case:
-        Return a empty dictionary if the string format of the data is inval_aid
+        Error handling case:
+        Return a empty dictionary if the string format of the data is invalid
         Empty dictionary is not None, it is a dictionary with no keys
     '''
-    # remove the pass bel_aow and start writing your code
+
+    # remove the pass below and start writing your code
+    
+
 def main():
     '''
-        handl_aing testcase input and printing output
+        handling testcase input and printing output
     '''
-    l_a = []
-    n_b = int(input())
-    for i in range(n_b):
-        list_a_input = input().spl_ait("follows")
-        l_a.extend(list_a_input)
-    print(create_social_a_network(l_a, n_b))
+    L = []
+    n = int(input())
+    for i in range(n):
+        list_input = input().split(" ")
+        L.extend(list_input)
+    print(create_social_network(L, n))
 main()
