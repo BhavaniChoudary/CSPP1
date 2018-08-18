ac76cccd052c49f8a45e3cf0849bd7d7d2c2e485
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-18 12:30:16 +0530

    " -> 1 of 1 passed. pylint: 9.57/10 "

--- cspp1-assignments/m18/p2/p2/search.py
+++ cspp1-assignments/m18/p2/p2/search.py
@@ -28,7 +28,7 @@
 
     Note: PyLint score need not be 10/10. Anything above 9.5 is good.
 '''
-    def search(search_index, query):
+def search(search_index, query):
     '''
         function to search through the search index and return the results
         split the query into lowercase words
