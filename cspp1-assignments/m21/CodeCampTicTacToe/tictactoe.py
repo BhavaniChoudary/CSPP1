571cbfaf4e6a4ef13323721be472a7a72dd9ff40
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-24 20:31:56 +0530

    " -> 0 of 14 passed. pylint: 1.18/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -25,27 +25,7 @@ def main():
     '''
         This is main function
     '''
-    winner = None
-    grid = []
-    for _ in range(0, 3, 1):
-        values = input().split(' ')
-        temp = []
-        for j in values:
-            temp.append(j)
-        grid.append(temp)
-    valid_grid = is_valid_grid(grid) and is_valid_game(grid)
+
     if valid_grid is True:
         winner = horizontal_check(grid)
-        if winner is None:
-            winner = verical_check(grid)
-        if winner is None:
-            winner = diagonal_check(grid)
-        if winner is None:
-            print("draw")
-        else:
-            print(winner)
-    else:
-        if not is_valid_grid(grid):
-            print("invalid input")
-        if  not is_valid_game(grid):
-            print("invalid game")
+        
