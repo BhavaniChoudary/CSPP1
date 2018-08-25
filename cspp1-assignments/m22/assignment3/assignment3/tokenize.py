d8f574d65eacde23d258f6d19bafce8ef453dbf9
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-25 13:30:55 +0530

    " -> 4 of 4 passed. pylint: 10.00/10 "

--- cspp1-assignments/m22/assignment3/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/assignment3/tokenize.py
@@ -1,30 +1,36 @@
-def tokenize(string, my_dict):
-    """
-    This functions takes a string as input and cleans it without special charecters and
-    returns a dictionary with freqncy of words
-    """
-    # global my_dict
-    for each_word in string:
-        if each_word in my_dict:
-            my_dict[each_word] += 1
-        elif each_word not in my_dict:
-            my_dict[each_word] = 1
-    return my_dict
+'''
+tokenize
+'''
+import re
 
-
+def tokenize(string):
+    '''
+    count
+    '''
+    a_b = []
+    a_c = []
+    dic_t = {}
+    for word in string:
+        wo_rd = (re.sub(r'[^\w\s]', '', word))
+        a_b.append(wo_rd)
+    for lin in a_b:
+        a_c.append(lin.split())
+    for line in a_c:
+        for words in line:
+            if words not in dic_t.keys():
+                dic_t[words] = 0
+            dic_t[words] += 1
+    return dic_t
 def main():
-    """
+    '''
     main function
-    """
-    my_dict = {}
-    no_of_lines = int(input())
-    while no_of_lines:
-        read_line = input()
-        my_string = re.sub("[^ 0-9A-Za-z]", "", read_line)
-        my_ans = tokenize(my_string.split(), my_dict)
-        no_of_lines -= 1
-    print(my_ans)
-
-
+    '''
+    lis_t = []
+    n_n = int(input())
+    for i in range(n_n):
+        del i
+        string = input()
+        lis_t.append(string)
+    print(tokenize(lis_t))
 if __name__ == '__main__':
     main()
