18f627b36c4889183e9226ee67abc038e9b572ff
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-25 12:29:25 +0530

    " -> 0 of 4 passed. pylint: -17.33/10 "

--- cspp1-assignments/m22/assignment3/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/assignment3/tokenize.py
@@ -4,19 +4,25 @@ each word
 '''
 
 def tokenize(string):
-	'''
-	Tokenize
-	'''
-	dict_n = {}
+    '''
+    Tokenize
+    '''
+    dict_n = {}
     for _ in range(0, len(list_a)):
         if list_a[i] not in dict_n:
             dict_n[list_a[i]] = list_a[i+1].split(",")
     return dict_n
 
 def main():
-	'''
-	Main function
-	'''
+    '''
+    Main function
+    '''
+    l_n = []
+    n_n = int(input())
+    for _ in range(n_n):
+        list_a_input = input().split(" ")
+        l_n.extend(list_a_input)
+    print(tokenize(l_n, n_n))
 
 if __name__ == '__main__':
     main()
