5cf8b4c319186077a8cff8f9e96f887d368ac921
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-08-25 11:19:57 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- cspp1-assignments/m22/assignment2/assignment2/clean_input.py
+++ cspp1-assignments/m22/assignment2/assignment2/clean_input.py
@@ -9,7 +9,7 @@ def clean_string(string):
     '''
     for char in string:
         if char in "!@#$%^&*()":
-            print(" ")
+            print(clean_string)
     return string
 
 def main():
